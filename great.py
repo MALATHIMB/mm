e613f1d95cd3afb32f122729ececfe4f98a0b7b7
MALATHIMB <47211902+MALATHIMB@users.noreply.github.com>
2019-02-04 15:40:05 +0530

    Create great.py

---
+++ great.py
@@ -0,0 +1,10 @@
+print("enter the numbers")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if n1>n2 and n1>n3:
+    print(n1,"n1 is greatest")
+elif n2>n3:
+    print(n2,"n2 is greatest")
+else:
+    print(n3,"n3 is greatest")
