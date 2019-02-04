7b435349d3a3e4776b06efb214e1c4e2ad7267ac
MALATHIMB <47211902+MALATHIMB@users.noreply.github.com>
2019-02-04 15:51:24 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,12 @@
+print("enter the year")
+year=int(input())
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("leap year")
+        else:
+            print("not")
+    else:
+        print("leap year")
+else:
+    print("not")
